a9f9d9400caccd436853f5b14d5e764e103c5381
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2014-01-17 19:34:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snapwall::Application.config.secret_key_base = 'd7c49183e5383c16a0e04d3e86e58a9bbcc313721acc6f217e499cdb72312a1315b427377970937a162e843d746a095985ecf070922853a834a9963ba3ec1c60'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snapwall::Application.config.session_store :cookie_store, key: '_snapwall_session'
