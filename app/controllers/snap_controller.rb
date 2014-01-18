72b890fdfb880719bc96a45b84ff465531b3b71b
Scott Opell <me@scottopell.com>
2014-01-18 01:27:41 -0500

    ideas are here, not working code

--- app/controllers/snap_controller.rb
+++ app/controllers/snap_controller.rb
@@ -2,6 +2,8 @@ class SnapController < ApplicationController
 
   def index
     threshold = 30.minutes
+
+
     if params.has_key? :not
       not_ids = params[:not].split(",")
       @snap = Snap.where(
@@ -14,5 +16,29 @@ class SnapController < ApplicationController
     end
 
     @snap = FakeSnap.new("http://lorempixel.com/cymbaler/540/960/", 10) if @snap.nil?
+    cleanup(threshold)
+  end
+
+
+  private 
+  def cleanup threshold
+    # won't work. will send duplicates
+    old_snaps = Snap.best_of.where('created_at < ?', threshold.ago)
+    old_snaps.each do |snap|
+      if snap.best_of?
+        send_best_of snap
+      end
+    end
+  end
+
+  def send_best_of snap
+    canvas = Magick::Image.new(960, 540){self.background_color = 'white'}
+    gc = Magick::Draw.new
+    gc.pointsize(50)
+    gc.text(30,70, "You made Best Of with #{snap.likes}!".center(14))
+
+    gc.draw(canvas)
+    
+    str = StringIO.open(canvas.to_blob)
   end
 end
