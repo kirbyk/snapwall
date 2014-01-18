7019129701905375ed1f30c03a07b40b678410f6
Rick Button <me@rickbutton.me>
2014-01-18 11:44:17 -0500

    add blacklists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140118065126) do
+ActiveRecord::Schema.define(version: 20140118163957) do
+
+  create_table "blacklists", force: true do |t|
+    t.string   "username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
